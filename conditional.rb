2a0890f4e0e5d05dd696f560d8a9a98a1d745912
Maximilianoho <maximho20@gmail.com>
2015-02-03 21:39:47 +0100

    conditional file

---
+++ conditional.rb
@@ -0,0 +1,13 @@
+puts "Who do you want to know more about: Gustavo, Duilio or Max"
+answer = gets.chomp
+
+if answer == "Gustavo" 
+	puts "Gustavo's favourite things to do in Berlin are go dancing"
+elsif answer == "Duilio" 
+	puts "Duilio's favourite things to do in Berlin is drinking esspreso"
+elsif answer == "Max" 
+	puts "Max's favourite things to in Berlin is drinking beer"
+else 
+	puts "I didn't mentione that name "
+end
+
